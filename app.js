24bf6553ec8d44ad5fbcba178445392dacfe3dc7
Yogesh Lakhotia <mr.yogesh@outlook.com>
2018-07-16 02:14:32 +0530

    - 404, 500 error handling - removed morgan, using bunyan for all logging

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let morgan = require('morgan');
-let methodOverride = require('method-override');
 let compression = require('compression');
 let config = require('./config');
-let log = require('./logger/log');
-let errorHandler = require('./errors/handler');
-var routes = require('./routes');
 let app = express();
 
 app.use(compression());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(methodOverride());
-app.use(errorHandler.logErrors);
-app.use(errorHandler.clientError);
-app.use(errorHandler.error);
-
 let fs = require('fs');
 let logDirectory = __dirname + '/log';
 
 // Log all requests in a daily log file using morgan
 if (fs.existsSync(logDirectory) === false) {
-  fs.mkdir(logDirectory);
+  fs.mkdirSync(logDirectory);
 }
-app.use(log.expressBunyan);
+app.use(require('./logger/log').expressBunyan);
+app.use('/', require('./routes'));
 
-app.use('/', routes);
+app.use((req, res, next) => {
+  res.send({
+    status: 404,
+    message: 'Not Found'
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({'error': err.stack});
+});
 
 app.listen(config.express.port, function() {
   console.log('Example app listening on port ' + config.express.port);
